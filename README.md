95819fa66f6f9774fd5d0dce5503bf603cca8eb9
Rubick <eithanllanosb@gmail.com>
2024-12-26 19:51:37 -0500

    set up spring initializr and sql scripts

---
+++ README.md
@@ -0,0 +1,5 @@
+# 15-ManyToManyMapping-demo
+The following dependencies were included using Spring Initializr:
+
+- MySQL Driver
+- Spring Data JPA
---
+++ manyToManyJpaMapping/src/main/java/com/springCourse/manyToManyJpaMapping/ManyToManyJpaMappingApplication.java
@@ -0,0 +1,13 @@
+package com.springCourse.manyToManyJpaMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyJpaMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyJpaMappingApplication.class, args);
+	}
+
+}
